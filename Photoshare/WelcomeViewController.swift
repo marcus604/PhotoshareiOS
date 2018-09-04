9ffecf3a07b965607d4c51828bec885777b81175
marcus604 <m.liebermann@me.com>
2018-09-04 00:27:13 -0700

    username and password textfield
    method to check if any text is valid, changes border if no text

--- Photoshare/WelcomeViewController.swift
+++ Photoshare/WelcomeViewController.swift
@@ -18,6 +18,13 @@ class WelcomeViewController: UIViewController {
     
     private var photoshare = Photoshare()
     
+    @IBOutlet var settingFields: [UITextField]!
+    @IBOutlet weak var hostNameTextField: UITextField!
+    @IBOutlet weak var portTextField: UITextField!
+    @IBOutlet weak var allowSelfSignedCertSwich: UISwitch!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     
     @IBAction func hostNameFieldDidChange(_ sender: UITextField) {
         print("host changed")
@@ -44,7 +51,50 @@ class WelcomeViewController: UIViewController {
         }
     }
     
+    //Verifies that textfield has text in it, if not sets border to red
+    private func checkForTextIn(textField : UITextField) -> Bool{
+        if textField.text?.isEmpty ?? true {
+            textField.layer.borderColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+            textField.layer.borderWidth = 1.0
+            return false
+        } else {
+            textField.layer.borderWidth = 0
+            return true
+        }
+    }
+    
     @IBAction func connectButton(_ sender: UIButton) {
+        //Check if all fields are valid
+        var fieldsValid = true
+        for textField in settingFields {
+            if !checkForTextIn(textField: textField){
+                fieldsValid = false
+            } else {
+                let text = textField.text!
+                switch textField {
+                case hostNameTextField:
+                    photoshare.set(hostName: text)
+                case portTextField:
+                    photoshare.set(port: text)
+                case userNameTextField:
+                    photoshare.set(hostName: text)
+                case passwordTextField:
+                    photoshare.set(password: text)
+                default:
+                    break
+                }
+            }
+        }
+        
+        if fieldsValid {
+            //need to set the self signed certs
+            
+            print("ready to go")
+        }
+        
+        
+        
+        
         
     }
     
